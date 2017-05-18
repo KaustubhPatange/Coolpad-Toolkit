87a1668feb21697f93054ef31427ccbc3b286fe9
Kaustubh Patange <DeveloperKP16@gmail.com>
2017-05-18 20:25:06 +0530

    Add files via upload

---
+++ tools/msg.bat
@@ -0,0 +1,84 @@
+@echo off
+echo.
+echo Your Phone will reboot for 17 times .. MAybe 
+ping localhost -n 3 >nul
+echo.
+echo This Whole Process May Take 15-20 mins :(
+ping localhost -n 4 >nul
+echo.
+echo So Just Chill.. 
+ping localhost -n 4 >nul
+echo.
+echo Don't Unplug your phone from PC.
+ping localhost -n 8 >nul
+echo.
+echo Maybe You're gettings bored..
+ping localhost -n 4 >nul
+echo.
+Echo Even I am.. :p
+ping localhost -n 3 >nul
+echo.
+echo Shit Man This is taking soo much time.. :(
+ping localhost -n 4 >nul
+echo.
+echo What should we do now..
+ping localhost -n 4 >nul
+echo.
+echo Don't worry this is normal.. It will take time :p
+ping localhost -n 4 >nul
+echo.
+echo How much time I don't know..
+ping localhost -n 7 >nul
+echo.
+echo Don't Stare at me.. I have done everything correct..
+ping localhost -n 3 >nul
+echo.
+echo Nothing is wrong in my process..
+ping localhost -n 9 >nul
+echo.
+echo You know Now really I am getting bored..
+ping localhost -n 6 >nul
+echo.
+echo Shit This much time..
+echo.
+ping localhost -n 4 >nul
+echo Don't worry this is normal.. It will take time :p
+ping localhost -n 4 >nul
+echo.
+echo I am Leaving now.. OK
+ping localhost -n 4 >nul
+echo.
+echo What should I wait..
+ping localhost -n 4 >nul
+echo.
+echo But I need to go..
+ping localhost -n 4 >nul
+echo.
+echo Its Urgent..
+ping localhost -n 4 >nul
+echo.
+echo I need to set some options..
+ping localhost -n 4 >nul
+echo.
+echo Just chill brother..
+ping localhost -n 4 >nul
+echo.
+echo Everything is gonna right..
+ping localhost -n 4 >nul
+echo.
+echo If you want my Support whatsapp me @ +91 7208565164
+ping localhost -n 4 >nul
+echo.
+echo I'll meet you there.. OK
+echo.
+ping localhost -n 4 >nul
+echo Now I am going Ok..
+echo.
+ping localhost -n 4 >nul
+echo Bye..
+echo.
+ping localhost -n 4 >nul
+echo.
+ping localhost -n 4 >nul
+echo I: RETURNING TO PREVIOUS WINDOW..
+ping localhost -n 3 >nul
